637e1d6d0629735ed64dadfba8e3dfa10e28c476
naomifleurnoel <nperenboom@hotmail.com>
2023-08-24 20:28:49 +0200

    Add project files.

---
+++ PetCare/Models/PersonModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace PetCare.Models
+{
+    public class PersonModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+        
+        public string? LastName { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? HouseNr { get; set; }
+
+        public string? Postcode { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Email { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+    }
+}
---
+++ PetCare/Models/PetModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetCare.Models;
+
+public class PetModel
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string? Name { get; set; }
+    
+    public DateTime DateOfBirth { get; set; } = DateTime.Now;
+
+    public string? Gender { get; set; }
+
+    public DateTime PlaceableFrom { get; set; }
+
+    public bool CanJoinCats { get; set; } = false;
+
+    public bool CanJoinDogs{ get; set; } = false;
+
+    public bool Indoor { get; set; } = true;
+
+    public string? PetSort { get; set; }
+
+    public bool Availible { get; set; } = false;
+}
